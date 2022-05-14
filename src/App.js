5d97a46694ab57e6956fb631455a0c88f52a4ebd
Eduardo Benitez <eduardombc96@gmail.com>
2022-05-14 18:29:52 -0500

    Updating display message

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lalo Benitez's future portfolio website.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/LaloBenitez/lalobenitez-portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to source-code
         </a>
       </header>
     </div>
